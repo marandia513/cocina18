4257708851d2363c247bf432f1a8be241b743950
MarianoLopez <marianooomcpe@gmail.com>
2024-05-10 21:06:12 -0300

    Cambios y agregado de nuevas recetas

--- Js/main.js
+++ Js/main.js
@@ -3,9 +3,37 @@ const next = document.querySelector('.next')
 const slider = document.querySelector('.slider')
 
 prev.addEventListener('click', () =>{
-    slider.scrollLeft -= 300
+    slider.scrollLeft -= 315
 })
 
 next.addEventListener('click', () =>{
-    slider.scrollLeft += 300
+    slider.scrollLeft += 315
+})
+
+// Controles Postres
+
+const prev2 = document.querySelector('.prev2')
+const next2 = document.querySelector('.next2')
+const slider2 = document.querySelector('.slider2')
+
+prev2.addEventListener('click', () =>{
+    slider2.scrollLeft -= 300
+})
+
+next2.addEventListener('click', () =>{
+    slider2.scrollLeft += 300
+})
+
+// Controles Panaderia
+
+const prev3 = document.querySelector('.prev3')
+const next3 = document.querySelector('.next3')
+const slider3 = document.querySelector('.slider3')
+
+prev3.addEventListener('click', () =>{
+    slider3.scrollLeft -= 300
+})
+
+next3.addEventListener('click', () =>{
+    slider3.scrollLeft += 300
 })
